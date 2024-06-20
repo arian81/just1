90ec577dccf9bc5b8ce4abdd1542562b6f9811d3
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-06-20 16:20:44 -0400

    chore: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,24 @@ import { GeistSans } from "geist/font/sans";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { env } from "~/env";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+
+if (typeof window !== "undefined") {
+  // checks that we are client-side
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,7 +28,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <main className={GeistSans.className}>
-        <Component {...pageProps} />
+        <PostHogProvider client={posthog}>
+          <Component {...pageProps} />
+        </PostHogProvider>
       </main>
     </SessionProvider>
   );
