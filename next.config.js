4b5e6c2d7700969ea42fa5ebdacd6eb6def9ede6
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-06-21 00:08:51 -0400

    chore: proxy analytics

--- next.config.js
+++ next.config.js
@@ -17,6 +17,19 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
   transpilePackages: ["geist"],
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,8 +13,10 @@ import "~/styles/globals.css";
 if (typeof window !== "undefined") {
   // checks that we are client-side
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    // api_host: env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
+    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
     loaded: (posthog) => {
       if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
     },
