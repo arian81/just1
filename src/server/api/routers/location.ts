54c559435fa1528d957913d82176850d5f10dec8
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-07-08 23:07:39 -0400

    chore: remove api key

--- src/server/api/routers/location.ts
+++ src/server/api/routers/location.ts
@@ -6,21 +6,11 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
-async function getNearbyRestaurants(lat: number, lng: number, radius = 1000) {
-  const url = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=restaurant&key=AIzaSyACpHAiT5WAIXMazTWGKXa8S936HAw1OEU`;
-  const response = await fetch(url);
-  // const data = await response.json();
-  console.log("data", response);
-  console.log(url);
-  // return data;
-  return {};
-}
-
-export const locationRouter = createTRPCRouter({
-  saveLocation: publicProcedure
-    .input(z.object({ latitude: z.number(), longitude: z.number() }))
-    .mutation(async ({ ctx, input }) => {
-      console.log("location", input);
-      return getNearbyRestaurants(input.latitude, input.longitude);
-    }),
-});
+// export const locationRouter = createTRPCRouter({
+//   saveLocation: publicProcedure
+//     .input(z.object({ latitude: z.number(), longitude: z.number() }))
+//     .mutation(async ({ ctx, input }) => {
+//       console.log("location", input);
+//       return getNearbyRestaurants(input.latitude, input.longitude);
+//     }),
+// });
